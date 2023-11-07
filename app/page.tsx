41e7e6d2469749264c72b09f0522d13c21a81d3b
LucasSantamaria76 <luc_1976@hotmail.com>
2023-11-07 11:44:38 -0300

    fix import

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import AcmeLogo from "@/app/ui/acme-logo";
 
 import Link from "next/link";
-import { lusitana } from "./ui";
 import Image from "next/image";
+import { lusitana } from "./fonts";
 export default function Page() {
   return (
     <main className='flex min-h-screen flex-col p-6'>
